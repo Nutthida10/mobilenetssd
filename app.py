9d8555df308a0be601097672c8ef08f29377a60e
Nutthida10 <88994820+Nutthida10@users.noreply.github.com>
2021-12-07 15:23:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -150,36 +150,36 @@ def event_handle(event):
      if msgType == "text":
          msg = str(event["message"]["text"])
          if (msg == "สวัสดี") :
-            replyObj = TextSendMessage(text="สวัสดีจ้า")
-            line_bot_api.reply_message(rtoken, replyObj)
+           replyObj = TextSendMessage(text="สวัสดีจ้า")
+           line_bot_api.reply_message(rtoken, replyObj)
          elif (msg == "กินข้าวยัง") :
-            replyObj = TextSendMessage(text="ยังเลย")
-            line_bot_api.reply_message(rtoken, replyObj)
-         elif (msg == "ง่วงนอนไหม") :
-            replyObj = TextSendMessage(text="ง่วงมาก")
-            line_bot_api.reply_message(rtoken, replyObj)
+           replyObj = TextSendMessage(text="ยังเลย")
+           line_bot_api.reply_message(rtoken, replyObj)
+         elf (msg == "ง่วงนอนไหม") :
+           replyObj = TextSendMessage(text="ง่วงมาก")
+           line_bot_api.reply_message(rtoken, replyObj)
          else:
-            headers = request.headers 
-            json_headers = json.dumps({k:v for k, v in headers.items()})
-            '''
-            json_line = request.get_json(force=False,cache=False)
-            json_line = json.dumps(json_line)
-            decoded = json.loads(json_line) 
-            crl= pycurl.Curl() 
-            crl.setopt( crl.URL, "https://bots.dialogflow.com/line/s--chlm/webhook") 
-            crl.setopt( crl.POST, 1) 
-            crl.setopt( crl.BINARYTRANSFER, true) 
-            crl.setopt( crl.POSTFIELDS, decoded)
-            crl.setopt( crl.HTTPHEADER, json_headers)
-            crl.setopt( crl.SSL_VERIFYHOST, 2) 
-            crl.setopt( crl.SSL_VERIFYPEER, 1)
-            crl.setopt( crl.FOLLOWLOCATION, 1)
-            crl.setopt( crl.RETURNTRANSFER, 1) 
-            crl.perform() 
-            crl.close() 
-            '''
-            replyObj = TextSendMessage(text=json_headers) 
-            line_bot_api.reply_message(rtoken, replyObj)
+           headers = request.headers 
+           json_headers = json.dumps({k:v for k, v in headers.items()})
+           '''
+           json_line = request.get_json(force=False,cache=False)
+           json_line = json.dumps(json_line)
+           decoded = json.loads(json_line) 
+           crl= pycurl.Curl() 
+           crl.setopt( crl.URL, "https://bots.dialogflow.com/line/s--chlm/webhook") 
+           crl.setopt( crl.POST, 1) 
+           crl.setopt( crl.BINARYTRANSFER, true) 
+           crl.setopt( crl.POSTFIELDS, decoded)
+           crl.setopt( crl.HTTPHEADER, json_headers)
+           crl.setopt( crl.SSL_VERIFYHOST, 2) 
+           crl.setopt( crl.SSL_VERIFYPEER, 1)
+           crl.setopt( crl.FOLLOWLOCATION, 1)
+           crl.setopt( crl.RETURNTRANSFER, 1) 
+           crl.perform() 
+           crl.close() 
+           '''
+           replyObj = TextSendMessage(text=json_headers) 
+           line_bot_api.reply_message(rtoken, replyObj)
      elif msgType == "image":
         try:
             message_content = line_bot_api.get_message_content(event['message']['id'])
